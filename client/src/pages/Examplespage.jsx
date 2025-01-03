3943e1f929b47e5d1fa88163ba8355eecb4faa7e
Andrii Salohub <andrejsalogub3@gmail.com>
2025-01-03 17:48:01 +0200

    Change some infomration in documentaion

--- client/src/pages/Examplespage.jsx
+++ client/src/pages/Examplespage.jsx
@@ -1,4 +1,3 @@
-// TODO: Пофіксити проблему з валідацією назви при редагуванні
 import { useState, useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
